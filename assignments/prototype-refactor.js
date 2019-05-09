aaa5afacc73a6dc969c268f5507e6c7d44c071ee
nanoblit <nanoblit@users.noreply.github.com>
2019-05-09 13:08:25 +0200

    Change prototypes to classes

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -16,15 +16,17 @@
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
 
-function GameObject(props) {
-    this.createdAt = props.createdAt;
-    this.name = props.name;
-    this.dimensions = props.dimensions;
-}
+class GameObject {
+    constructor(props) {
+        this.createdAt = props.createdAt;
+        this.name = props.name;
+        this.dimensions = props.dimensions;
+    }
 
-GameObject.prototype.destroy = function() {
-    return `${this.name} was removed from the game.`;
-};
+    destroy() {
+        return `${this.name} was removed from the game.`;
+    }
+}
 
 /*
   === CharacterStats ===
@@ -33,21 +35,21 @@ GameObject.prototype.destroy = function() {
   * should inherit destroy() from GameObject's prototype
 */
 
-function CharacterStats(props) {
-    GameObject.call(this, props);
-    this.healthPoints = props.healthPoints;
-}
-
-CharacterStats.prototype = Object.create(GameObject.prototype);
-
-CharacterStats.prototype.takeDamage = function(damage = 10) {
-    this.healthPoints -= damage;
-    let returnString = `${this.name} took damage.`;
-    if (this.healthPoints <= 0) {
-        returnString += ` They fainted!`;
+class CharacterStats extends GameObject {
+    constructor(props) {
+        super(props);
+        this.healthPoints = props.healthPoints;
     }
-    return returnString;
-};
+
+    takeDamage(damage = 10) {
+        this.healthPoints -= damage;
+        let returnString = `${this.name} took damage.`;
+        if (this.healthPoints <= 0) {
+            returnString += ` They fainted!`;
+        }
+        return returnString;
+    }
+}
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -59,18 +61,18 @@ CharacterStats.prototype.takeDamage = function(damage = 10) {
   * should inherit takeDamage() from CharacterStats
 */
 
-function Humanoid(props) {
-    CharacterStats.call(this, props);
-    this.team = props.team;
-    this.weapons = props.weapons;
-    this.language = props.language;
+class Humanoid extends CharacterStats {
+    constructor(props) {
+        super(props);
+        this.team = props.team;
+        this.weapons = props.weapons;
+        this.language = props.language;
+    }
+
+    greet() {
+        return `${this.name} offers a greeting in ${this.language}`;
+    }
 }
-
-Humanoid.prototype = Object.create(CharacterStats.prototype);
-
-Humanoid.prototype.greet = function() {
-    return `${this.name} offers a greeting in ${this.language}`;
-};
 
 /*
  * Inheritance chain: GameObject -> CharacterStats -> Humanoid
@@ -137,31 +139,31 @@ console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
 // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.
 // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
 // * Create two new objects, one a villain and one a hero and fight it out with methods!
-function Hero(props) {
-    this.attack = props.attack;
-    Humanoid.call(this, props);
+class Hero extends Humanoid {
+    constructor(props) {
+        super(props);
+        this.attack = props.attack;
+    }
+
+    attackEnemy(foe) {
+        console.log(
+            `${this.name} uses ${this.attack.name} on ${foe.name} dealing ${
+                this.attack.damage
+            } damage!`
+        );
+        console.log(foe.takeDamage(this.attack.damage));
+    }
 }
 
-Hero.prototype = Object.create(Humanoid.prototype);
+class Villain extends Humanoid {
+    constructor(props) {
+        super(props);
+    }
 
-Hero.prototype.attackEnemy = function(foe) {
-    console.log(
-        `${this.name} uses ${this.attack.name} on ${foe.name} dealing ${
-            this.attack.damage
-        } damage!`
-    );
-    console.log(foe.takeDamage(this.attack.damage));
-};
-
-function Villain(props) {
-    Humanoid.call(this, props);
+    runAway() {
+        console.log(`${this.name} tries to run away!`);
+    }
 }
-
-Villain.prototype = Object.create(Humanoid.prototype);
-
-Villain.prototype.runAway = function() {
-    console.log(`${this.name} tries to run away!`);
-};
 
 const allMight = new Hero({
     createdAt: new Date(),
